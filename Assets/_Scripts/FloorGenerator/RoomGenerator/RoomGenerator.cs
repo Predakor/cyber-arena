3b7ddfa8c2bd5be8676feb6868b1a164292d8416
Predakor <patrykbusko@gmail.com>
2025-03-06 15:37:50 +0100

    Add: Generator now leaves space for the doors

--- Assets/_Scripts/FloorGenerator/RoomGenerator/RoomGenerator.cs
+++ Assets/_Scripts/FloorGenerator/RoomGenerator/RoomGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomGenerator : MonoBehaviour {
@@ -10,6 +11,8 @@ public class RoomGenerator : MonoBehaviour {
     [SerializeField] int _wallSize = 10;
 
     [SerializeField] Vector3 _wallRotationOffest = Vector3.zero;
+
+    [SerializeField] List<bool> _doors = new();
 
 #if UNITY_EDITOR
 
@@ -26,18 +29,32 @@ public class RoomGenerator : MonoBehaviour {
         GenerateRoom();
     }
 
+    void OnValidate() {
+        _doors.Clear();
+        for (int i = 0; i < _roomStats.sides; i++) {
+            bool random = Random.Range(0, 4) == 1;
+            _doors.Add(random);
+        }
+    }
+
 #endif
 
     public void LoadData(RoomData roomData) {
         _roomStats = roomData.stats;
         _roomPrefabs = roomData.prefabs;
+        _doors = new List<bool>() { true, false, true, false };
+
+
     }
 
     [ContextMenu("GenerateRoom")]
     public void GenerateRoom() {
         SpawnFloors();
         SpawnWalls();
+        SpawnDoors();
     }
+
+
 
     [ContextMenu("GenerateFlors")]
     void SpawnFloors() {
@@ -79,25 +96,48 @@ public class RoomGenerator : MonoBehaviour {
             Vector3 wallPosition = new(0, 0, 0);
 
             for (int i = 0; i < _roomStats.sides; i++) {
+                bool hasDoor = _doors[i];
+
                 float angle = rotationBase * i;
                 float posX = roomRadius * Mathf.Cos(Mathf.Deg2Rad * angle);
                 float posZ = roomRadius * Mathf.Sin(Mathf.Deg2Rad * angle);
 
                 wallPosition.Set(posX, 0, posZ);
 
-                InstantiateWall(wallScale, wallPosition, GetRandomWall());
+                if (hasDoor) {
+                    Vector3 wallSegmentScale = wallScale;
+                    float wallRemainingSpace = wallLength - _doorSize;
+                    float scaleRatio = wallRemainingSpace / wallLength / 2;
+                    float offsetValue = wallRemainingSpace / 4 + (_doorSize / 2);
+
+                    Vector3 offset = new(0, 0, offsetValue);
+                    wallSegmentScale.z *= scaleRatio;
+
+                    InstantiateWall(wallSegmentScale, wallPosition, GetRandomWall())
+                        .transform.Translate(offset, Space.Self);
+                    InstantiateWall(wallSegmentScale, wallPosition, GetRandomWall())
+                        .transform.Translate(-offset, Space.Self);
+
+                }
+                else {
+                    InstantiateWall(wallScale, wallPosition, GetRandomWall());
+                }
+
             }
         }
 
-        void InstantiateWall(Vector3 wallScale, Vector3 wallPosition, GameObject randomWall) {
+        GameObject InstantiateWall(Vector3 wallScale, Vector3 wallPosition, GameObject randomWall) {
             GameObject generatedWall = Instantiate(randomWall, transform);
             generatedWall.transform.localPosition = wallPosition;
             generatedWall.transform.LookAt(transform.position);
             generatedWall.transform.Rotate(_wallRotationOffest);
             generatedWall.transform.localScale = wallScale;
+            return generatedWall;
         }
     }
-
+    void SpawnDoors() {
+        throw new System.NotImplementedException();
+    }
 
 
     GameObject GetRandomWall() => GetRandomPrefab(_roomPrefabs.wallPrefabs);
