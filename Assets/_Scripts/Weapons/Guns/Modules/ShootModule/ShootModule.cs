1b7ebb5c7421efacee75343564bb5cc1a0540542
Predakor <patrykbusko@gmail.com>
2025-04-07 14:20:45 +0200

    Fix: enemies not taking damage

--- Assets/_Scripts/Weapons/Guns/Modules/ShootModule/ShootModule.cs
+++ Assets/_Scripts/Weapons/Guns/Modules/ShootModule/ShootModule.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
 public abstract class ShootModule : MonoBehaviour {
-    [SerializeField] protected Projectile _projectile;
     public abstract void Shoot(Projectile projectile);
 }
